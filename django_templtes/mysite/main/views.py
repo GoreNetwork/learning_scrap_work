7f8772e37d0a2445015af0a7a9dfe8a12d270fc3
daniel himes <dhimes@gmail.com>
2021-06-01 12:57:46 -0400

    template working

--- django_templtes/mysite/main/views.py
+++ django_templtes/mysite/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial
 
 
 def homepage(request):
-    return HttpResponse("Butt")
+    # return HttpResponse("Butt")
+    return (render(request=request,
+                   template_name='main/home.html',
+                   context={'tutorials': Tutorial.objects.all}))
 
 # Create your views here.
