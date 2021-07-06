4f4bc32b4afe82f71198f9ac720cb69fd56f7499
daniel himes <dhimes@gmail.com>
2021-07-06 15:23:48 -0400

    mostly working now

--- dolt_git_db/work_with_dolt_db.py
+++ dolt_git_db/work_with_dolt_db.py
@@ -3,7 +3,6 @@ import sys
 import json
 # import netmiko
 
-
 #Start server with "dolt sql-server" run in the folder where the repo is
 #Gives an output of Starting server with Config HP="localhost:3306"|U="root"|P=""|T="28800000"|R="false"|L="info"
 # Documentation here https://docs.dolthub.com/interfaces/sql/clients
@@ -53,37 +52,42 @@ class dolt_work:
     def close_conn(self):
         self.cursor.close()
         self.connection.close()
+    def drop_table(self, table_name):
+        self.cursor.execute(f"drop table {table_name};")
+        self.connection.commit()
+        return
 
 
 
 test_db = dolt_work(ip, port, db, db_user, db_password)
 
-try:
-    # build_table_statment= "drop table json_spew;"
-    # print(test_db.run_sql_statment(build_table_statment))
-    print(test_db.run_sql_statment('show tables;'))
-    build_table_statment= "create table json_spew(json_feild text, prime_key int not null auto_increment, primary key(prime_key))"
-    print(test_db.run_sql_statment(build_table_statment))
+# try:
 
-except:
-    pass
+print(test_db.drop_table('json_spew'))
+
+# build_table_statment= "create table json_spew(json_feild text, prime_key int not null auto_increment, primary key(prime_key))"
+# print(test_db.run_sql_statment(build_table_statment))
+print(test_db.run_sql_statment('show tables;'))
+
+# except:
+    # pass
 
 
 # build_table_statment= "drop table json_spew;"
 # print(test_db.run_sql_statment(build_table_statment))
 
-crap_data = '{"name":"John", "age":30, "car":null}'
+# crap_data = '{"name":"John", "age":30, "car":null}'
 
-json_data = json.loads(crap_data)
+# json_data = json.loads(crap_data)
 
 
 
-for x in range(1,3):
-    json_data['age']=json_data['age']+1
-    junk_json_data=json.dumps(json_data)
-    insert_statment = f'''INSERT INTO json_spew (json_feild)VALUES ('{junk_json_data}');'''
-    print (insert_statment)
-    test_db.insert_to_db(insert_statment)
+# for x in range(1,3):
+#     json_data['age']=json_data['age']+1
+#     junk_json_data=json.dumps(json_data)
+#     insert_statment = f'''INSERT INTO json_spew (json_feild)VALUES ('{junk_json_data}');'''
+#     print (insert_statment)
+#     test_db.insert_to_db(insert_statment)
 test_db.close_conn()
 
 
