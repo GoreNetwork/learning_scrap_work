05bf27a2f8ca7dca3c5ce45401d969bf836b52d4
daniel himes <dhimes@gmail.com>
2021-07-12 11:46:52 -0400

    update

--- dolt_git_db/work_with_dolt_db.py
+++ dolt_git_db/work_with_dolt_db.py
@@ -2,13 +2,14 @@ import mysql.connector
 import sys
 import json
 # import netmiko
+from pprint import pprint
 
 #Start server with "dolt sql-server" run in the folder where the repo is
 #Gives an output of Starting server with Config HP="localhost:3306"|U="root"|P=""|T="28800000"|R="false"|L="info"
 # Documentation here https://docs.dolthub.com/interfaces/sql/clients
 
 
-ip = '127.0.0.1'
+ip = '192.168.0.10'
 port = '3306'
 db = 'dolt'
 db_user = 'root'
@@ -56,26 +57,39 @@ class dolt_work:
         self.cursor.execute(f"drop table {table_name};")
         self.connection.commit()
         return
+    
 
 
 
 test_db = dolt_work(ip, port, db, db_user, db_password)
-
-try:
-
-    # print(test_db.drop_table('json_spew'))
-    print(test_db.run_sql_statment('show tables;'))
-    build_table_statment= "create table json_spew(json_feild text, prime_key int not null auto_increment, primary key(prime_key))"
-    build_table_statment= "create table bubba_joe(json_feild text, prime_key int not null auto_increment, primary key(prime_key))"
-    print(test_db.run_sql_statment(build_table_statment))
-    print(test_db.run_sql_statment('show tables;'))
-
-except:
-    pass
+needed_tables1 = ["bubba_joe", "json_spew"]
+needed_tables2 = ["not_bubba_joe", "not_json_spew"]
 
 
-# build_table_statment= "drop table bubba_joe;"
-print(test_db.drop_table("bubba_joe"))
+def build_tables(test_db,needed_tables):
+    for table in needed_tables:
+        build_junk_table = f"create table {table}(json_feild text, prime_key int not null auto_increment, primary key(prime_key))"
+        try:
+           print(test_db.run_sql_statment(build_junk_table))
+        except:
+            pass
+
+def drop_tables(test_db, needed_tables):
+    for table in needed_tables:
+        try:
+            test_db.drop_table(table)
+        except:
+            pass
+
+
+# drop_tables(test_db, needed_tables1)
+# build_tables(test_db,needed_tables1)
+# build_tables(test_db,needed_tables2)
+drop_tables(test_db, needed_tables2)
+
+print(test_db.run_sql_statment('show tables;'))
+
+
 
 crap_data = '{"name":"John", "age":30, "car":null}'
 
@@ -87,8 +101,13 @@ for x in range(1,3):
     json_data['age']=json_data['age']+1
     junk_json_data=json.dumps(json_data)
     insert_statment = f'''INSERT INTO bubba_joe (json_feild)VALUES ('{junk_json_data}');'''
-    print (insert_statment)
-    test_db.insert_to_db(insert_statment)
+    # print (insert_statment)
+    # test_db.insert_to_db(insert_statment)
+select_from_table = f'select * from bubba_joe;'
+try:
+    pprint (test_db.run_sql_statment(select_from_table))
+except:
+    pass
 test_db.close_conn()
 
 
